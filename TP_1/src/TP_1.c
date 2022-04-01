d37ef49af75ef6a88667a5f07bfeef9fc4a5aa5a
gemonieto <gemonieto@gmail.com>
2022-04-01 12:30:20 -0300

    primer commit

---
+++ TP_1/src/TP_1.c
@@ -0,0 +1,14 @@
+/*
+ ============================================================================
+ Name        : TP_1.c
+ Author      : Germán Nieto
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
